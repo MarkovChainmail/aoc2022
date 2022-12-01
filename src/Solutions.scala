3106700e7db83f66a0778e37e95b0843656fc6c0
Pascal <e.a.hildebrand@student.tudelft.nl>
2022-12-01 16:07:22 +0100

    let's ignore 'source not closed' problems for now

--- src/Solutions.scala
+++ src/Solutions.scala
@@ -1,22 +1,18 @@
 import scala.io.Source
-import scala.util.Using
 
 @main def run() : Unit =
   // Put the one you want to run here
   println(ex1B())
 
 def ex1A() : Int =
-  Using(Source.fromFile("puzzles/puzz1")) {reader => reader.getLines
+  Source.fromFile("puzzles/puzz1").getLines
     .foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
-    )
-  }.get.max
+    ).max
 
 def ex1B() : Int =
-  Using(Source.fromFile("puzzles/puzz1")) {reader => reader.getLines
-    .foldLeft(List(0))((l: List[Int], c: String) => c match
+  Source.fromFile("puzzles/puzz1").getLines.foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
-    )
-  }.get.sorted.takeRight(3).sum
+    ).sorted.takeRight(3).sum
