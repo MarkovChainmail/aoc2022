526a1c8e37bf58b2c853b9eb353ecf720b9c897f
Pascal <e.a.hildebrand@student.tudelft.nl>
2022-12-01 15:31:08 +0100

    Solutions day 1

---
+++ src/Solutions.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+import scala.util.Using
+
+@main def run() : Unit =
+  // Put the one you want to run here
+  println(ex1B())
+
+def ex1A() : Int =
+  Using(Source.fromFile("puzzles/puzz1")) {reader =>
+    reader.getLines().foldLeft(List(0))((l: List[Int], c: String) => c match {
+      case "" => 0 :: l
+      case c => c.toInt + l.head :: l.tail
+    })
+  }.get.max
+
+def ex1B() : Int =
+  Using(Source.fromFile("puzzles/puzz1")) {reader =>
+    reader.getLines().foldLeft(List(0))((l: List[Int], c: String) => c match {
+      case "" => 0 :: l
+      case c => c.toInt + l.head :: l.tail
+    })
+  }.get.sorted.takeRight(3).sum
