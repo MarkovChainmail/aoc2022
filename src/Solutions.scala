51dce4d692778ca84b949a093826290a7cde986d
Pascal <e.a.hildebrand@student.tudelft.nl>
2022-12-01 16:30:30 +0100

    a little perfectionism, as a treat

--- src/Solutions.scala
+++ src/Solutions.scala
@@ -1,18 +1,25 @@
-import scala.io.Source
-import java.nio.file.{Files, Path}
-@main def run() : Unit =
-  // Put the one you want to run here
-  println(ex1B())
+import jdk.internal.net.http.hpack.Huffman.Reader
 
-def ex1A() : Int =
-  Source.fromFile("puzzles/puzz1").getLines
+import scala.io.Source
+import scala.util.Using
+@main def run() : Unit =
+  // Name of file
+  val filename = "puzzles/puzz1"
+
+  Using(Source.fromFile(filename)) {reader => println(
+    // Name of exercise
+    ex1B(reader)
+  )}
+
+def ex1A(reader: Source) : Int =
+  reader.getLines
     .foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
     ).max
 
-def ex1B() : Int =
-  Source.fromFile("puzzles/puzz1").getLines
+def ex1B(reader: Source) : Int =
+  reader.getLines
     .foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
