dd4351b115008235df7846e893adb0d9cd1a1b77
Pascal <e.a.hildebrand@student.tudelft.nl>
2022-12-01 16:01:28 +0100

    we can make her cleaner

--- src/Solutions.scala
+++ src/Solutions.scala
@@ -6,16 +6,16 @@ import scala.util.Using
   println(ex1B())
 
 def ex1A() : Int =
-  Using(Source.fromFile("puzzles/puzz1")) {reader =>
-    reader.getLines().foldLeft(List(0))((l: List[Int], c: String) => c match
+  Using(Source.fromFile("puzzles/puzz1")) {reader => reader.getLines
+    .foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
     )
   }.get.max
 
 def ex1B() : Int =
-  Using(Source.fromFile("puzzles/puzz1")) {reader =>
-    reader.getLines().foldLeft(List(0))((l: List[Int], c: String) => c match
+  Using(Source.fromFile("puzzles/puzz1")) {reader => reader.getLines
+    .foldLeft(List(0))((l: List[Int], c: String) => c match
       case "" => 0 :: l
       case c => c.toInt + l.head :: l.tail
     )
